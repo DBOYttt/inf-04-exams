8fad4fd98adf6ac9f095e37b0f08bef14a53158c
D-BOY <andrzejn041@gmail.com>
2024-10-10 12:03:15 +0200

    xaml and c# file for dotnet MAUI

---
+++ INF.04-02-23.01-SG/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+
+namespace INF._04_02_23._01_SG_mobileapp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private ObservableCollection<string> notes;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            notes = new ObservableCollection<string>
+            {
+                "Notatka 1",
+                "Notatka 2",
+                "Notatka 3"
+            };
+
+
+            NotesListView.ItemsSource = notes;
+        }
+
+
+        private void OnAddButtonClicked(object sender, EventArgs e)
+        {
+
+            notes.Add(NewItemEntry.Text);
+
+
+            NewItemEntry.Text = string.Empty;
+        }
+    }
+
+}
